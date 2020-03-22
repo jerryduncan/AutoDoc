dce0cd93423cf3378938bed62dfdef66022ccb9d
manuelExtra <40274371+ManuelExtra@users.noreply.github.com>
2020-03-22 16:14:58 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+const users = [
+  {
+    name: "user",
+    password: "user"
+  },
+  {
+    name: "example",
+    password: "qwerty"
+  }
+];
+module.exports = { users: users };
---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+
+const middleware = require("./middleware");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+
+app.use('/api', require('./routes/api'));
+
+
+
+const PORT = 6000;
+
+app.listen(PORT);
+console.log("api runnging on port " + PORT + ": ");
